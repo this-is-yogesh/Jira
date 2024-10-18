9481e31f3e25730e228b54ffbf249def213b01bb
Yogesh Singh <ayogesh1214@gmail.com>
2024-10-18 10:13:31 +0530

    buttons added and input,select height increased

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="test" size="sm">
+    <div className="flex gap-4">
+      <Input />
+      <Button size="sm"> Primary</Button>
+      <Button variant="destructive" size="sm">
         {" "}
-        Click Me
+        Destructive
       </Button>
-      <p className="text-red-400 font-semibold text-xl">Text Red</p>
+      <Button variant="outline" size="sm">
+        {" "}
+        Click 3
+      </Button>
+      <Button variant="muted" size="sm">
+        {" "}
+        Click 4
+      </Button>
+      <Button variant="territary" size="sm">
+        {" "}
+        Click 5
+      </Button>
     </div>
   );
 }
